077072ca5217f094ddfaa28fc6cd625b3af66094
jalalSeidi <jalalseidi7@gmail.com>
2022-11-15 23:28:11 +0000

    Maximum Number of Balloons

---
+++ MaxBalloonNum/MaxBalloonNum/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public int maxNumberOfBalloons(String text) {
+        Map<Character, Long> textFrequencyMap = getFrequencyMap(text);
+        Map<Character, Long> ballonFrequencyMap = getFrequencyMap("balloon");
+        return ballonFrequencyMap.keySet().stream()
+                .map(k ->
+                        (int) (textFrequencyMap.getOrDefault(k, 0L) / ballonFrequencyMap.get(k)))
+                .min(Integer::compare)
+                .orElse(0);
+    }
+
+    private Map<Character, Long> getFrequencyMap(String s) {
+        return s.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), HashMap::new, Collectors.counting()));
+    }
+    
+}
