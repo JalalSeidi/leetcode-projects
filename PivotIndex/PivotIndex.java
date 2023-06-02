d58205a6e80cdd4c795c94596e596b3348e55a22
jalalSeidi <jalalseidi7@gmail.com>
2023-06-02 23:28:39 +0100

    Finding the pivot index in an array

---
+++ PivotIndex/PivotIndex.java
@@ -0,0 +1,19 @@
+public class PivotIndex {
+    public static int pivotIndex(int [] nums) {
+        int totalSum = 0;
+        int leftsum = 0;
+        int rightSum = 0;
+        for (int ele : nums)
+            totalSum += ele;
+        for (int i = 0; i < nums.length; leftsum += nums[i++])
+            if (leftsum * 2 == totalSum - nums[i])
+                return i;
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {1,7,3,6,5,6};
+        System.out.println((pivotIndex(nums)));
+    }
+}
